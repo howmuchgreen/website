a4d1a91d72db1a91b8a1bd6743f2c3ebdd831bdc
Basile Bruneau <basile@bruneau.email>
2022-01-15 23:39:25 +0100

    feat: handle arrows keys on home page

--- components/ResultThing/index.tsx
+++ components/ResultThing/index.tsx
@@ -6,6 +6,8 @@ interface Props {
   result: Thing;
 }
 
+// Sources: https://www.carbonbrief.org/analysis-why-children-must-emit-eight-times-less-co2-than-their-grandparents
+// Sources: https://www.lemonde.fr/planete/article/2019/04/11/combien-de-co2-pourrez-vous-emettre-dans-votre-vie-si-le-rechauffement-est-contenu-a-1-5-degre_5448606_3244.html
 const CARBON_YEARLY_TODAY_GRAMS = 4_800_000;
 const CARBON_YEARLY_2050_GRAMS = 1_900_000;
 
