969ba8f3a2b64ceaa801cef59c8e25fa92ab355d
Basile Bruneau <basilebruneau@gmail.com>
2022-02-05 22:33:14 +0100

    feat: add About links

---
+++ app/components/About/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import * as S from "./styles";
+
+export const About = () => (
+  <S.Container>
+    <a href="https://github.com/howmuchgreen/website" target="_blank">
+      GitHub
+    </a>{" "}
+    /{" "}
+    <a href="https://twitter.com/howmuchgreen" target="_blank">
+      Twitter
+    </a>
+  </S.Container>
+);
